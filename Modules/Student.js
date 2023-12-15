a80dcb8a0fbb6f2e02fba59744c2c446cbadff6d
harikakshinthala <harikaakshinthala1@gmail.com>
2023-12-15 18:19:51 +0530

    final

---
+++ Modules/Student.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const studentSchema = mongoose.Schema(
+    {
+        StudentName:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        StudentEmail:{
+            type:String,
+            required:true
+        },
+        CurrentMentor:{
+            type:String
+        },
+        PreviousMentor:{
+            type:String
+        }
+    }
+)
+
+export const Student = mongoose.model("Student",studentSchema)
+//export const Student= mongoose.model("students",studentSchema)
